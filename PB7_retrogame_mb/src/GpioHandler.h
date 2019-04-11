c710920bebe2d93b7f4b6e7e6e2e2d5b019543dd
Pelle van Gils <pelle@vangils.xyz>
2019-04-11 11:57:34 +0200

    comment GPioHandler

--- PB7_retrogame_mb/src/GpioHandler.h
+++ PB7_retrogame_mb/src/GpioHandler.h
@@ -14,11 +14,23 @@
 
 class GpioHandler {
 public:
+    // Initializes GPIO device and sets up an interrupt callback.
+    // param:   gpioId:     ID of the GPIO device. (see xparameters.h)
+    //          intrId:     ID of the interrupt vector for the GPIO device.
+    //          callback:   callback to the function to call on interrupt.
+    //          Intc:       pointer to the interrupt device object to register the
+    //                      interrupt to.
 	GpioHandler(u8 gpioId, u8 intrId, void	(*callback)(u8 data), XIntc *Intc);
 	virtual ~GpioHandler();
 
 public:
+    // Initialize and enable interrupt controller.
+    // param:   Intc:   pointer to XIntc object.
+    //          intrId: ID of the interrupt controller to be initialized (see xparameters.h)
+    // return:  true on success; false otherwise.
 	static 	bool InitializeInterruptController(XIntc *Intc, u8 intrId);
+    // write data to GPIO channel 1
+    // param:   data:   Mask for the GPIO channel
 	void 	write(u32 data);
 
 private:
